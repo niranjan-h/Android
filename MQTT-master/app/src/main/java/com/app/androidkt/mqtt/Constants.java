6c87544df44724b53fa71de0858d96598c8cd229
niranjan-h <niranjan.h@icloud.com>
2019-04-05 12:19:13 +0530

    New Andro

---
+++ MQTT-master/app/src/main/java/com/app/androidkt/mqtt/Constants.java
@@ -0,0 +1,15 @@
+package com.app.androidkt.mqtt;
+
+/**
+ * Created by brijesh on 20/4/17.
+ */
+
+public class Constants {
+
+    public static final String MQTT_BROKER_URL = "tcp://broker.hivemq.com:1883";
+
+    public static final String PUBLISH_TOPIC = "SKSV/topic";
+
+    public static final String CLIENT_ID = "SKSV";
+}
+
